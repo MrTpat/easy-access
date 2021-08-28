95cd552866c23a0fd4cf1e2f2332632a6cd891a8
Tilak Patel <ptilak142@gmail.com>
2021-08-28 13:16:36 -0400

    initial

---
+++ README.md
@@ -0,0 +1,11 @@
+## Simple utility to quickly access common commands
+
+## Requirements:
+- zsh
+- fzf
+
+## Install:
+Run the setup.sh file. Make sure to read the output.
+
+## Usage:
+Simply run the "ez" command
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+cat shell_script >> ~/.zshrc
+touch ~/useful_commands
+echo "Please source your ~/.zshrc file, and run the ez command"
+echo "Make sure that the useful_commands file in your home directory is filled out!"
